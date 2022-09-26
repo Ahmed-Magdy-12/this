a81ec4d283e47289774c306660c224efa998e68f
Ahmed-Magdy-12 <mhmedshalaby1234@gmail.com>
2022-09-26 22:14:46 +0200

    anything

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+let clickbar = document.getElementById('clickbar');
+let showbar = document.getElementById('showbar');
+
+/* ================ When Click Menu  =============== */
+clickbar.addEventListener('click', () => {
+    showbar.classList.toggle('showbar')
+})
